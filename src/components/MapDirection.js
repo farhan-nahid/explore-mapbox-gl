cee0079a882d82974f146908ca2d7cf3c3bf19bc
gesora96 <nahidahmed9933@gmail.com>
2021-11-18 19:06:03 +0600

    first commit

---
+++ src/components/MapDirection.js
@@ -0,0 +1,9 @@
+import mapboxgl from 'mapbox-gl';
+import React from 'react';
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL_TOKEN;
+
+const MapDirection = () => {
+  return <div></div>;
+};
+
+export default MapDirection;
